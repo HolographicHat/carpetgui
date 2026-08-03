3f0a229b00f7c755a5fcf65927d3c1e5fb62a9e1
HolographicHat <58809250+HolographicHat@users.noreply.github.com>
2026-08-03 22:18:31 +0800

    Update stonecutter.gradle.kts

--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("dev.kikugie.stonecutter")
-    id("net.fabricmc.fabric-loom") version "1.17-SNAPSHOT" apply false
+    id("net.fabricmc.fabric-loom") version "1.17" apply false
 }
 
 stonecutter active "26.2"
